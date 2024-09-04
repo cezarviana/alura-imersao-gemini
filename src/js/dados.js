31a5e188fde03be67ece6a0a4c2ceb32e831baa9
Cezar Viana <cezar.viana@gmail.com>
2024-09-04 17:33:01 -0300

    <add>: dados.js e app.js e organização das pastas

---
+++ src/js/dados.js
@@ -0,0 +1,14 @@
+let atleta = [
+    {  titulo : "Rayssa Leal",
+        descrição : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {   titulo : "Rebeca Andrade",
+        descrição : "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {   titulo : "Beatriz Souza",
+        descrição : "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos, trazendo orgulho para o país. Sua força e técnica a colocaram entre as melhores do mundo.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+];
