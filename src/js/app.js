c5bb3d0b04741e09a5fabc9029ca73cb15cf2676
Cezar Viana <cezar.viana@gmail.com>
2024-09-06 15:27:26 -0300

    <add>: function pesquisar() e inserir dados via JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,26 @@
-console.log(atleta[1]);
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+
+    // Itera sobre cada atleta na lista de atletas
+    for (let atleta of atletas) {
+        // Constrói o HTML para um item de resultado, formatando com as informações do atleta
+        resultados += `
+                    <div class="item-resultado">
+                        <h2>
+                            <a href="#" target="_blank">${atleta.titulo}</a>
+                        </h2>
+                        <p class="descricao-meta">${atleta.descricao}</p> 
+                        <a href="${atleta.link}" target="_blank">Mais informações</a>
+                    </div>
+        `;
+    }
+
+    // Atribui o HTML construído à seção de resultados, atualizando a página
+    section.innerHTML = resultados;
+}
+
+
